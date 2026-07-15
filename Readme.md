bb6e8264dd03d0bd6db29a3331d536dd7d6d03ea
ehasun15019 <ehasunulislam0202@gmail.com>
2026-07-15 23:32:50 +0600

    create the profile schema

---
+++ Readme.md
@@ -0,0 +1,1 @@
+1. "user" model create kora hoise akon ... profile model ta create kore connect korte hobe .. jate 1st user tkei profile make hoi 
---
+++ prisma/migrations/20260715173149_profile_schema/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "profile" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "linkedin" TEXT,
+    "github" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profile_userId_key" ON "profile"("userId");
+
+-- AddForeignKey
+ALTER TABLE "profile" ADD CONSTRAINT "profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
